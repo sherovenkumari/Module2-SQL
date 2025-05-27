864ee1f5c18fc70c438da31758f7a337d70ad72f
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 10:11:40 +0530

    Add files via upload

---
+++ Que7.sql
@@ -0,0 +1,17 @@
+
+-- Q7: Low Feedback Alerts
+-- Show users who gave feedback rating < 3, with their comments and event title.
+
+SELECT 
+    u.full_name,
+    e.title AS event_title,
+    f.rating,
+    f.comments
+FROM 
+    Feedback f
+JOIN 
+    Users u ON f.user_id = u.user_id
+JOIN 
+    Events e ON f.event_id = e.event_id
+WHERE 
+    f.rating < 3;
