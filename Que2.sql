e433830dacd4904bbf0cccda769237b64af9d6bc
sherovenkumari <sherovenkumari311@gmail.com>
2025-05-27 10:08:45 +0530

    Add files via upload

---
+++ Que2.sql
@@ -0,0 +1,18 @@
+
+-- Q2: Top Rated Events
+-- Identify events with highest average rating, considering only those with at least 10 feedbacks.
+
+SELECT 
+    e.title,
+    AVG(f.rating) AS avg_rating,
+    COUNT(f.feedback_id) AS total_feedbacks
+FROM 
+    Events e
+JOIN 
+    Feedback f ON e.event_id = f.event_id
+GROUP BY 
+    e.event_id
+HAVING 
+    COUNT(f.feedback_id) >= 10
+ORDER BY 
+    avg_rating DESC;
